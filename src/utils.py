725cded54ea9f1fe49b6615dcc9321edf25a937c
Rei Matsuzaki <matsuzaki.rei@gmail.com>
2015-09-26 18:09:01 +0900

    add synthesis_mat function and its test

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+from scipy.sparse import coo_matrix
+
+
 def flatten(xs):
     return reduce(lambda a, b: a+b, xs)
 
@@ -8,3 +11,27 @@ def uniq(xs):
 
 def with_index(xs):
     return zip(xs, range(len(xs)))
+
+
+def synthesis_mat(a, b):
+    """ gives conbination of two matrix a and b
+
+    Parameters
+    ----------
+    a, b : scipy.sparse.occ_matrix
+       a and b must be square matrix and
+
+    Returns
+    -------
+    out : scipy.sparse.occ_matrix
+    """
+    (na_row, na_col) = a.shape
+    (nb_row, nb_col) = b.shape
+
+    n_row = na_row * nb_row
+    n_col = na_col * nb_col
+
+    col = [i+na_col*j for j in b.col for i in a.col]
+    row = [k+na_row*l for l in b.row for k in a.row]
+    data = [ik*jl for jl in b.data for ik in a.data]
+    return coo_matrix((data, (row, col)))
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,7 @@
 import unittest
 from utils import *
+import numpy as np
+from scipy.sparse import coo_matrix
 
 
 class TestUtils(unittest.TestCase):
@@ -7,10 +9,42 @@ class TestUtils(unittest.TestCase):
         pass
 
     def test_flatten(self):
-        self.assertEqual([1, 2, 3, 4], flatten([[1], [2, 3], [4]]))
+        self.assertEqual([1, 2, 3, 4],
+                         flatten([[1], [2, 3], [4]]))
 
     def test_uniq(self):
-        self.assertEqual([1, 2, 3], uniq([1, 2, 2, 1, 3, 3]))
+        self.assertEqual([1, 2, 3],
+                         uniq([1, 2, 2, 1, 3, 3]))
+
+    def test_synthesis_mat(self):
+        """
+        m1 =
+        [1, 0, 2, -1]
+        [0, 0, 3,  0]
+        [4, 5, 6, -2]
+        m2 =
+        [1,-2, -5]
+        [0, 3, -6]
+        """
+        row = np.array([0, 0, +0, 1, 2, 2, 2, 2])
+        col = np.array([0, 2, +3, 2, 0, 1, 2, 3])
+        dat = np.array([1, 2, -1, 3, 4, 5, 6, -2])
+        m1 = coo_matrix((dat, (row, col)))
+        row = np.array([+0, +0, +0, +1, +1])
+        col = np.array([+0, +1, +2, +1, +2])
+        dat = np.array([+1, -2, -5, +3, -6])
+        m2 = coo_matrix((dat, (row, col)))
+
+        self.assertEqual((3, 4), m1.shape)
+        self.assertEqual((2, 3), m2.shape)
+
+        m3 = synthesis_mat(m1, m2).toarray()
+        self.assertEqual((6, 12), m3.shape)
+        self.assertAlmostEqual(2.0, m3[0, 2])
+        self.assertAlmostEqual(3.0, m3[1, 2])
+        self.assertAlmostEqual(-6.0, m3[1, 4+2])
+        self.assertAlmostEqual(+9.0, m3[3+1, 4+2])
+
 
 if __name__ == '__main__':
     unittest.main()
