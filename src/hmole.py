e066d4aed6bb6c6c7cee2b1d41e4040357c50132
Rei Matsuzaki <matsuzaki.rei@gmail.com>
2015-09-28 14:46:25 +0900

    rewritten hmole.py/mat_h2_plus using y1mat concept

--- src/hmole.py
+++ src/hmole.py
@@ -1,10 +1,8 @@
 import numpy as np
 from numpy import pi, sqrt
-from sympy.physics.wigner import gaunt
-from scipy.sparse import bmat
-from bspline_bind import ra_inv
-from utils import flatten, uniq
-from angmoment import ls_non_zero_YYY
+from scipy.sparse import bmat, coo_matrix
+from utils import flatten, uniq, synthesis_mat
+from angmoment import ls_non_zero_YYY, y1mat_Yqk
 
 
 def mat_h2_plus(bond_length, bspline_set, l_list):
@@ -29,24 +27,72 @@ def mat_h2_plus(bond_length, bspline_set, l_list):
     # compute r1 matrix  (B_i|O|B_j)
     # (-1/2 d^2/dr^2, 1, 1/r^2, {s^L/g^{L+1} | L<-l_list})
     rs = bspline_set.xs
-    d2_mat = bspline_set.d2_mat()
-    r2_mat = bspline_set.v_mat(1.0/(rs*rs))
-    s_mat = bspline_set.s_mat()
+    d2_rmat = bspline_set.d2_mat()
+    r2_rmat = bspline_set.v_mat(1.0/(rs*rs))
+    s_rmat = bspline_set.s_mat()
     tmp_L_list = uniq(flatten([ls_non_zero_YYY(L1, L2)
                                for L1 in l_list for L2 in l_list]))
-    #    ra_s_L = {L: bspline_set.v_mat(ra_inv(rs, L, bond_length/2.0))
-    #              for L in tmp_L_list}
-    ra_s_L = {L: bspline_set.en_mat(L, bond_length/2.0) for L in tmp_L_list}
+    en_r1mat_L = {L: bspline_set.en_mat(L, bond_length/2.0)
+                  for L in tmp_L_list}
+
+    # compute y1 matrix (Y_L1|P_L(w_A)|Y_L2)
+    en_y1mat_L = {L: coo_matrix([[np.sqrt(4.0*np.pi/(2*L+1)) *
+                                  y1mat_Yqk((L1, 0), (L, 0), (L2, 0))
+                                  for L1 in l_list]
+                                 for L2 in l_list])
+                  for L in tmp_L_list}
+    LL_y1mat = coo_matrix(np.diag([1.0*L*(L+1) for L in l_list]))
+    diag_y1mat = coo_matrix(np.diag([1 for L in l_list]))
+
+    # compute r1y1 matrix
+    h_r1y1mat = (synthesis_mat(-0.5*d2_rmat, diag_y1mat) +
+                 synthesis_mat(+0.5*r2_rmat, LL_y1mat) - 2.0 *
+                 sum([synthesis_mat(en_r1mat_L[L], en_y1mat_L[L])
+                      for L in tmp_L_list]))
+    s_r1y1mat = synthesis_mat(s_rmat, diag_y1mat)
+
+    return (h_r1y1mat, s_r1y1mat)
+
+
+def mat_h2_plus_old(bond_length, bspline_set, l_list):
+    """ gives hamiltonian matrix and overlap matrix of hydrogem molecule ion
+
+    Parameters
+    ----------
+    bond_length : Double
+         bond length of hydrogen molecular ion
+    bspline_set : BSplineSet
+    l_list: list of non negative integer
+         list of angular quauntum number to use
+
+    Returns
+    -------
+    h_mat : numpy.ndarray
+        hamiltonian matrix
+    s_mat : numpy.ndarray
+        overlap pmatrix
+    """
+
+    # compute r1 matrix  (B_i|O|B_j)
+    # (-1/2 d^2/dr^2, 1, 1/r^2, {s^L/g^{L+1} | L<-l_list})
+    rs = bspline_set.xs
+    d2_rmat = bspline_set.d2_mat()
+    r2_rmat = bspline_set.v_mat(1.0/(rs*rs))
+    s_rmat = bspline_set.s_mat()
+    tmp_L_list = uniq(flatten([ls_non_zero_YYY(L1, L2)
+                               for L1 in l_list for L2 in l_list]))
+    en_r1mat_L = {L: bspline_set.en_mat(L, bond_length/2.0)
+                  for L in tmp_L_list}
 
     # compute r1Y matrix (B_iY_L1M1|O|B_jY_L2M2)
     def one_block(L1, L2):
         v = -2.0*sum([sqrt(4.0*pi/(2*L+1)) *
-                      float(gaunt(L1, L, L2, 0, 0, 0, 12)) *
-                      ra_s_L[L]
+                      y1mat_Yqk((L1, 0), (L, 0), (L2, 0)) *
+                      en_r1mat_L[L]
                       for L in ls_non_zero_YYY(L1, L2)])
         if L1 == L2:
             L = L1
-            t = -0.5 * d2_mat + L*(L+1)*0.5*r2_mat
+            t = -0.5 * d2_rmat + L*(L+1)*0.5*r2_rmat
             return t+v
         else:
             return v
@@ -54,7 +100,7 @@ def mat_h2_plus(bond_length, bspline_set, l_list):
     H_mat = bmat([[one_block(L1, L2)
                    for L1 in l_list]
                   for L2 in l_list])
-    S_mat = bmat([[s_mat if L1 == L2 else None
+    S_mat = bmat([[s_rmat if L1 == L2 else None
                    for L1 in l_list]
                   for L2 in l_list])
     return (H_mat, S_mat)
--- tests/test_hmole.py
+++ tests/test_hmole.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 import scipy.sparse.linalg as la
 from bspline import *
 from hmole import *
@@ -15,9 +16,18 @@ class TestHMole(unittest.TestCase):
         num = 140
         r_h_mole = 2.0
         basis_set = BSplineSet(order, lin_knots(0.0, rmax, num))
-        (h, s) = mat_h2_plus(r_h_mole, basis_set, [0, 2, 4, 6, 8])
+        t0 = time.clock()
+        (h, s) = mat_h2_plus_old(r_h_mole, basis_set, [0, 2, 4, 6, 8])
+        t1 = time.clock()
         (es, vecs) = la.eigsh(h, 5, s, sigma=-2.0)
         self.assertTrue(abs(es[0] - (-1.1026)) < 0.01)
+
+        t2 = time.clock()
+        (h, s) = mat_h2_plus(r_h_mole, basis_set, [0, 2, 4, 6, 8])
+        t3 = time.clock()
+        (es, vecs) = la.eigsh(h, 5, s, sigma=-2.0)
+        self.assertTrue(abs(es[0] - (-1.1026)) < 0.01)
+        print (t1-t0, t3-t2)
 
 if __name__ == '__main__':
     unittest.main()
