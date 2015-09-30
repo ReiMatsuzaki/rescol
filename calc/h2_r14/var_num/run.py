0bb2e9c9bdf8c42ae2b8945d851e9b8aad6473dd
Rei Matsuzaki (rccls) <matsuzaki.rei@sepia.chem.keio.ac.jp>
2015-09-30 09:44:26 +0900

    some calculation

--- calc/h2_r14/var_num/run.py
+++ calc/h2_r14/var_num/run.py
@@ -8,7 +8,7 @@ import time
 # J.Sims, S.Hagstom, JCP *124*, 094101 (2006)
 ref_E0 = -1.174475174
 bond_length = 1.4
-
+rmax = 60.0
 
 # in reference paper
 # order = 8
@@ -16,24 +16,19 @@ bond_length = 1.4
 # num_knots =140
 #lmax = 8
 
-order = 4
-rmax = 60.0
-num_knots = 21
-lmax = 2
-
 vars = [ (lmax, num, order)
-         for lmax in [2]
+         for lmax in [4]
          for num in [20, 60, 100, 140]
-         for order in [4]]
+         for order in [6]]
 
 acc = []
 for (lmax, num, order) in vars:
     bspline_set = BSplineSet(order, lin_knots(0.0, rmax, num))
     y_list = [CoupledY((L1, L2), L, 0)
-              for L in range(2*lmax+1)
+              for L in range(lmax+1)
               for L1 in range(lmax+1)
               for L2 in range(lmax+1)
-              if triangle_q(L1, L2, L) and (L1+L2)%2==0]
+              if triangle_q(L1, L2, L) and (L1+L2)%2==0 and L1+L2<=lmax and L%2==0]
     t0 = time.clock()
     (h, s) = mat_h2(bond_length, bspline_set, y_list)
     t1 = time.clock()
@@ -42,7 +37,8 @@ for (lmax, num, order) in vars:
     acc.append([("lmax", lmax),
                 ("num", num),
                 ("order", order),
-                ("N", h.shape[0]),
+                ("Nbais", h.shape[0]),
+                ("Ndata", len(h.data)),
                 ("t_mat", t1-t0),
                 ("t_diag", t2-t1),
                 ("E0", es[0] + 1.0/bond_length)])
